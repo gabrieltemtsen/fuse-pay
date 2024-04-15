b7c68228c7c58679d9de56d19f899fa1de2aa0d3
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-04-15 16:48:12 +0100

    update

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -50,7 +50,7 @@ const Layout = ({ children }) => {
             <svg className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
             </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Profile</span>
+            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Loans</span>
         </Link>
     </div>
 </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -91,7 +91,7 @@ const { formatEther } = utils;
                         alt="celo"
                         className="ios:h-7 material:h-6 rounded-full"
                         src="/celo.png" />}
-                      class="text-xs bg-black rounded-full text-white px-4 py-1.5 mr-3"></Chip> <span class="text-sm font-medium">Powered by Celo</span>
+                      class="text-xs bg-black rounded-full text-white px-4 py-1.5 mr-3"></Chip> <span class="text-sm font-medium ml-2">Powered by Celo</span>
                     <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                   </a>
                   <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We Help you manage your Company`s Finance</h1>
@@ -103,7 +103,7 @@ const { formatEther } = utils;
                         <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                       </svg>
                     </Link>
-                    <Link className=" max-w-sm inline-flex justify-center items-center gap-x-1 text-center bg-gradient-to-tl from-blue-900 to-black-900 shadow-lg shadow-transparent hover:shadow-black-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800 mx-5" href="/dashboard">
+                    <Link className=" max-w-sm inline-flex justify-center items-center gap-x-1 text-center bg-gradient-to-tl from-blue-900 to-black-900 shadow-lg shadow-transparent hover:shadow-black-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800 mx-5" href="/company">
                       View company
                       <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
                         <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
@@ -114,7 +114,95 @@ const { formatEther } = utils;
 
                 </div>
               </section>
-            </div><Sheet
+
+              <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+ 
+  <div className="lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
+    <div className="lg:col-span-7">
+    
+      <div className="grid grid-cols-12 gap-2 sm:gap-6 items-center lg:-translate-x-10">
+        <div className="col-span-4">
+          <img className="rounded-xl"  src={"https://images.unsplash.com/photo-1606868306217-dbf5046868d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1981&q=80"} alt="Image Description" /> 
+        </div>
+        
+
+        <div className="col-span-3">
+          <img className="rounded-xl"  src={"https://images.unsplash.com/photo-1605629921711-2f6b00c6bbf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"} alt="Image Description" />
+        </div>
+        
+
+        <div className="col-span-5">
+          <img className="rounded-xl"  src="https://images.unsplash.com/photo-1600194992440-50b26e0a0309?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" alt="Image Description" />
+        </div>
+       
+      </div>
+      
+    </div>
+ 
+
+    <div className="mt-5 sm:mt-10 lg:mt-0 lg:col-span-5">
+      <div className="space-y-6 sm:space-y-8">
+        
+        <div className="space-y-2 md:space-y-4">
+          <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
+          Kickstart Payrolls and Secure Work Loans Effortlessly
+          </h2>
+          <p className="text-gray-500">
+            Use our services to inittiate payroll and transactions and loan requests for your Team/ workers.
+          </p>
+        </div>
+        
+
+       
+        <ul role="list" className="space-y-2 sm:space-y-4">
+          <li className="flex space-x-3">
+            
+            <svg className="flex-shrink-0 h-6 w-6 text-blue-600 dark:text-blue-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.1965 7.85999C15.1965 3.71785 11.8387 0.359985 7.69653 0.359985C3.5544 0.359985 0.196533 3.71785 0.196533 7.85999C0.196533 12.0021 3.5544 15.36 7.69653 15.36C11.8387 15.36 15.1965 12.0021 15.1965 7.85999Z" fill="currentColor" fillOpacity="0.1"/>
+              <path d="M10.9295 4.88618C11.1083 4.67577 11.4238 4.65019 11.6343 4.82904C11.8446 5.00788 11.8702 5.32343 11.6914 5.53383L7.44139 10.5338C7.25974 10.7475 6.93787 10.77 6.72825 10.5837L4.47825 8.5837C4.27186 8.40024 4.25327 8.0842 4.43673 7.87781C4.62019 7.67142 4.93622 7.65283 5.14261 7.83629L7.01053 9.49669L10.9295 4.88618Z" fill="currentColor"/>
+            </svg>
+            
+
+            <span className="text-sm sm:text-base text-gray-500">
+              <span className="font-bold">Low costs</span> – Covered by Celo 
+            </span>
+          </li>
+
+          <li className="flex space-x-3">
+            
+            <svg className="flex-shrink-0 h-6 w-6 text-blue-600 dark:text-blue-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.1965 7.85999C15.1965 3.71785 11.8387 0.359985 7.69653 0.359985C3.5544 0.359985 0.196533 3.71785 0.196533 7.85999C0.196533 12.0021 3.5544 15.36 7.69653 15.36C11.8387 15.36 15.1965 12.0021 15.1965 7.85999Z" fill="currentColor" fillOpacity="0.1"/>
+              <path d="M10.9295 4.88618C11.1083 4.67577 11.4238 4.65019 11.6343 4.82904C11.8446 5.00788 11.8702 5.32343 11.6914 5.53383L7.44139 10.5338C7.25974 10.7475 6.93787 10.77 6.72825 10.5837L4.47825 8.5837C4.27186 8.40024 4.25327 8.0842 4.43673 7.87781C4.62019 7.67142 4.93622 7.65283 5.14261 7.83629L7.01053 9.49669L10.9295 4.88618Z" fill="currentColor"/>
+            </svg>
+            
+
+            <span className="text-sm sm:text-base text-gray-500">
+              Seamless and Innovative Tech
+            </span>
+          </li>
+
+          <li className="flex space-x-3">
+        
+            <svg className="flex-shrink-0 h-6 w-6 text-blue-600 dark:text-blue-500" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.1965 7.85999C15.1965 3.71785 11.8387 0.359985 7.69653 0.359985C3.5544 0.359985 0.196533 3.71785 0.196533 7.85999C0.196533 12.0021 3.5544 15.36 7.69653 15.36C11.8387 15.36 15.1965 12.0021 15.1965 7.85999Z" fill="currentColor" fillOpacity="0.1"/>
+              <path d="M10.9295 4.88618C11.1083 4.67577 11.4238 4.65019 11.6343 4.82904C11.8446 5.00788 11.8702 5.32343 11.6914 5.53383L7.44139 10.5338C7.25974 10.7475 6.93787 10.77 6.72825 10.5837L4.47825 8.5837C4.27186 8.40024 4.25327 8.0842 4.43673 7.87781C4.62019 7.67142 4.93622 7.65283 5.14261 7.83629L7.01053 9.49669L10.9295 4.88618Z" fill="currentColor"/>
+            </svg>
+            
+
+            <span className="text-sm sm:text-base text-gray-500">
+              Scaling solutions <span className="font-bold">Improving Traditional systems</span>
+            </span>
+          </li>
+        </ul>
+       
+      </div>
+    </div>
+   
+  </div>
+  
+</div>
+            </div>
+            <Sheet
               className="pb-safe"
               opened={sheetOpened}
               onBackdropClick={() => setSheetOpened(false)}
